b08208e44f06a85151631d766730250833b3c191
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-08-31 21:35:34 +0530

    Add files via upload

--- src/Components/Routers/Router.js
+++ src/Components/Routers/Router.js
@@ -5,6 +5,7 @@ import React from "react";
 
 import { Route, Routes } from "react-router-dom";
 import About from "../Layout/About.js";
+import Tours from "../Layout/Home.js";
 import Productlist from "../Product/productlist.js";
 import { RoutePath } from "./Const.js";
 
@@ -12,6 +13,7 @@ const Routers = () => {
   return (
     <div>
       <Routes>
+      <Route path={RoutePath.Home} element={<Tours></Tours>} />
         <Route path={RoutePath.Home} element={<Productlist></Productlist>} />
         <Route path={RoutePath.Store} element={<Productlist />} />
         <Route path={RoutePath.About} element={<About></About>} />
