34c55d89e3d046796afd3e0dec5f428ef0c7e851
divyagurav <132669878+divyagurav@users.noreply.github.com>
2023-08-31 12:55:37 +0530

    Add files via upload

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -4,9 +4,35 @@ const CartItem = () => {
   const cart = [
     {
       title: "Colors",
+
       price: 100,
+
       imageUrl:
         "https://prasadyash2411.github.io/ecom-website/img/Album%201.png",
+
+      quantity: 2,
+    },
+
+    {
+      title: "Black and white Colors",
+
+      price: 50,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%202.png",
+
+      quantity: 3,
+    },
+
+    {
+      title: "Yellow and Black Colors",
+
+      price: 70,
+
+      imageUrl:
+        "https://prasadyash2411.github.io/ecom-website/img/Album%203.png",
+
+      quantity: 1,
     },
   ];
   return (
@@ -14,10 +40,7 @@ const CartItem = () => {
       {cart.map((item) => (
         <div className="cart-items" key={item.id}>
           <div className="cart-item-name">
-            <img
-              src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"
-              alt=""
-            ></img>
+            <img src={item.imageUrl} alt=""></img>
             <span className="title">{item.title}</span>
           </div>
           <div className="cart-items-price">{item.price}</div>
